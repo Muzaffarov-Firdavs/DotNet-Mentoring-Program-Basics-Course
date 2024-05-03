da4495e02df750f5a09c3f5e7b30bc9c305be5d6
Firdavs Muzaffarov <fmuzaffarov@ebsco.com>
2024-05-03 16:43:37 +0500

    init changes

---
+++ DebuggingFundamentals/Task1/Product.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Task1
+{
+    public class Product
+    {
+        public Product(string name, double price)
+        {
+            Name = name;
+            Price = price;
+        }
+
+        public string Name { get; set; }
+
+        public double Price { get; set; }
+
+
+        public override bool Equals(object obj)
+        {
+            if (obj is Product other)
+            {
+                return this.Name == other.Name && this.Price == other.Price;
+            }
+            return false;
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(Name, Price);
+        }
+    }
+}
