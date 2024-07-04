abde27cdef43783fa2921c266b7e5f3c75406225
Firdavs Muzaffarov <fmuzaffarov@ebsco.com>
2024-07-04 23:03:49 +0500

    fix bugs

--- SQL-DB/Task-1/SQL-DB/SQL-DB/Script.PostDeployment.sql
+++ SQL-DB/Task-1/SQL-DB/SQL-DB/Script.PostDeployment.sql
@@ -1,4 +1,19 @@
-﻿INSERT INTO Person (Id, FirstName, LastName) VALUES (1, 'John', 'Doe'), (2, 'Jane', 'Smith');
-INSERT INTO Address (Id, Street, City, State, ZipCode) VALUES (1, '123 Main St', 'Anytown', 'CA', '12345'), (2, '456 Maple St', 'Othertown', 'TX', '67890');
-INSERT INTO Company (Id, Name, AddressId) VALUES (1, 'TechCorp', 1), (2, 'HealthInc', 2);
-INSERT INTO Employee (Id, AddressId, PersonId, CompanyName, Position, EmployeeName) VALUES (1, 1, 1, 'TechCorp', 'Developer', 'John Doe'), (2, 2, 2, 'HealthInc', 'Nurse', NULL);
+﻿INSERT INTO Person (FirstName, LastName) 
+VALUES 
+('John', 'Doe'), 
+('Jane', 'Smith');
+
+INSERT INTO Address (Street, City, State, ZipCode) 
+VALUES 
+('123 Main St', 'Anytown', 'CA', '12345'), 
+('456 Maple St', 'Othertown', 'TX', '67890');
+
+INSERT INTO Company (Name, AddressId) 
+VALUES 
+('TechCorp', 1), 
+('HealthInc', 2);
+
+INSERT INTO Employee (AddressId, PersonId, CompanyName, Position, EmployeeName) 
+VALUES 
+(1, 1, 'TechCorp', 'Developer', 'John Doe'), 
+(2, 2, 'HealthInc', 'Nurse', NULL);
