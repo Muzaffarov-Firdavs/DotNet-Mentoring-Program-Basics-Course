9d8767fb9aa73ef933bdf11432acc4a81887bb35
Firdavs Muzaffarov <fmuzaffarov@ebsco.com>
2024-07-07 21:17:28 +0500

    configure logs to email

--- Logging/Task2/Logging/BrainstormSessions/Api/IdeasController.cs
+++ Logging/Task2/Logging/BrainstormSessions/Api/IdeasController.cs
@@ -1,12 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using BrainstormSessions.ClientModels;
+﻿using BrainstormSessions.ClientModels;
 using BrainstormSessions.Core.Interfaces;
 using BrainstormSessions.Core.Model;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace BrainstormSessions.Api
 {
--- Logging/Task2/Logging/BrainstormSessions/Program.cs
+++ Logging/Task2/Logging/BrainstormSessions/Program.cs
@@ -1,40 +1,57 @@
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Serilog;
+using Serilog.Sinks.Email;
+using Serilog.Events;
 using System;
+using BrainstormSessions;
 
-namespace BrainstormSessions
+public class Program
 {
-    public class Program
+    public static void Main(string[] args)
     {
-        public static void Main(string[] args)
+        var configuration = new ConfigurationBuilder()
+            .AddJsonFile("appsettings.json")
+            .Build();
+
+        var emailConnectionInfo = new EmailConnectionInfo
         {
-            Log.Logger = new LoggerConfiguration()
-                .MinimumLevel.Debug()
-                .WriteTo.File("Logs/log-.txt", rollingInterval: RollingInterval.Day)
-                .CreateLogger();
+            FromEmail = configuration["Serilog:WriteTo:0:Args:connectionInfo:FromEmail"],
+            ToEmail = configuration["Serilog:WriteTo:0:Args:connectionInfo:ToEmail"],
+            MailServer = configuration["Serilog:WriteTo:0:Args:connectionInfo:MailServer"],
+            EnableSsl = bool.Parse(configuration["Serilog:WriteTo:0:Args:connectionInfo:EnableSsl"]),
+            Port = int.Parse(configuration["Serilog:WriteTo:0:Args:connectionInfo:Port"]),
+            EmailSubject = configuration["Serilog:WriteTo:0:Args:connectionInfo:EmailSubject"],
+            NetworkCredentials = new System.Net.NetworkCredential(configuration["Serilog:WriteTo:0:Args:connectionInfo:NetworkCredentials:UserName"], configuration["Serilog:WriteTo:0:Args:connectionInfo:NetworkCredentials:Password"])
+        };
 
-            try
-            {
-                Log.Information("Starting web host");
-                CreateHostBuilder(args).Build().Run();
-            }
-            catch (Exception ex)
-            {
-                Log.Fatal(ex, "Host terminated unexpectedly");
-            }
-            finally
-            {
-                Log.CloseAndFlush();
-            }
+        Log.Logger = new LoggerConfiguration()
+            .WriteTo.Email(
+                emailConnectionInfo,
+                restrictedToMinimumLevel: LogEventLevel.Warning)
+            .CreateLogger();
+
+        try
+        {
+            Log.Information("Starting web host");
+            CreateHostBuilder(args).Build().Run();
         }
-
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .UseSerilog() // Add this line
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                });
+        catch (Exception ex)
+        {
+            Log.Fatal(ex, "Host terminated unexpectedly");
+        }
+        finally
+        {
+            Log.CloseAndFlush();
+        }
     }
+
+    public static IHostBuilder CreateHostBuilder(string[] args) =>
+        Host.CreateDefaultBuilder(args)
+            .UseSerilog() // Add this line to use Serilog
+            .ConfigureWebHostDefaults(webBuilder =>
+            {
+                webBuilder.UseStartup<Startup>();
+            });
 }
