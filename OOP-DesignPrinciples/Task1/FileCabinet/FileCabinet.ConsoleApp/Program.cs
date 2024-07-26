d97cb8d3b019707339a4b9308d751f539b06f80f
Firdavs Muzaffarov <fmuzaffarov@ebsco.com>
2024-07-26 10:07:10 +0500

    Done task 1

---
+++ OOP-DesignPrinciples/Task1/FileCabinet/FileCabinet.ConsoleApp/Program.cs
@@ -0,0 +1,46 @@
+﻿using FileCabinet.Domain.Models;
+using FileCabinet.Service.Interfaces;
+using FileCabinet.Service.Services;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string storagePath = @"C:\Users\Firdavs_Muzaffarov\Desktop\DotNet-Mentoring-Program-Basics-Course\Serialization\Task1\FileCabinet\FileCabinet.Data\JsonFiles";
+        IDocumentService documentService = new DocumentService(storagePath);
+
+        Console.WriteLine("Enter the document number to search:");
+        if (int.TryParse(Console.ReadLine(), out int documentNumber))
+        {
+            var document = documentService.SearchDocumentByNumber(documentNumber);
+            if (document != null)
+            {
+                PrintDocumentInfo(document);
+            }
+            else
+            {
+                Console.WriteLine("Document not found.");
+            }
+        }
+        else
+        {
+            Console.WriteLine("Invalid document number.");
+        }
+    }
+
+    static void PrintDocumentInfo(Document document)
+    {
+        switch (document)
+        {
+            case LocalizedBook localizedBook:
+                Console.WriteLine($"Localized Book: {localizedBook.Title}, ISBN: {localizedBook.ISBN}, Authors: {string.Join(", ", localizedBook.Authors)}, Pages: {localizedBook.NumberOfPages}, Original Publisher: {localizedBook.OriginalPublisher}, Country of Localization: {localizedBook.CountryOfLocalization}, Local Publisher: {localizedBook.LocalPublisher}, Date Published: {localizedBook.DatePublished}");
+                break;
+            case Book book:
+                Console.WriteLine($"Book: {book.Title}, ISBN: {book.ISBN}, Authors: {string.Join(", ", book.Authors)}, Pages: {book.NumberOfPages}, Publisher: {book.Publisher}, Date Published: {book.DatePublished}");
+                break;
+            case Patent patent:
+                Console.WriteLine($"Patent: {patent.Title}, Authors: {string.Join(", ", patent.Authors)}, Date Published: {patent.DatePublished}, Expiration Date: {patent.ExpirationDate}, Unique ID: {patent.UniqueId}");
+                break;
+        }
+    }
+}
---
+++ OOP-DesignPrinciples/Task1/FileCabinet/FileCabinet.Service/Services/DocumentService.cs
@@ -0,0 +1,35 @@
+﻿using FileCabinet.Domain.Models;
+using FileCabinet.Service.Interfaces;
+using System.Text.Json;
+
+namespace FileCabinet.Service.Services
+{
+    public class DocumentService : IDocumentService
+    {
+        private readonly string _storagePath;
+
+        public DocumentService(string storagePath)
+        {
+            _storagePath = storagePath;
+        }
+
+        public Document? SearchDocumentByNumber(int documentNumber)
+        {
+            var files = Directory.GetFiles(_storagePath, "*_#" + documentNumber + ".json");
+
+            if (files.Length == 0) return null;
+
+            var file = files[0];
+            var json = File.ReadAllText(file);
+            var documentType = Path.GetFileNameWithoutExtension(file).Split('_')[0];
+
+            return documentType switch
+            {
+                "book" => JsonSerializer.Deserialize<Book>(json),
+                "localizedbook" => JsonSerializer.Deserialize<LocalizedBook>(json),
+                "patent" => JsonSerializer.Deserialize<Patent>(json),
+                _ => null,
+            };
+        }
+    }
+}
