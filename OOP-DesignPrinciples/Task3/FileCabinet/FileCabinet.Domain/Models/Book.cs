110720649eba53c4542013ac769ba3d7e3588d05
Firdavs Muzaffarov <fmuzaffarov@ebsco.com>
2024-08-20 08:27:27 +0500

    refactor task 3

--- OOP-DesignPrinciples/Task3/FileCabinet/FileCabinet.Domain/Models/Book.cs
+++ OOP-DesignPrinciples/Task3/FileCabinet/FileCabinet.Domain/Models/Book.cs
@@ -3,10 +3,7 @@
     public class Book : Document
     {
         public string ISBN { get; set; }
-        public string Title { get; set; }
-        public string[] Authors { get; set; }
         public int NumberOfPages { get; set; }
         public string Publisher { get; set; }
-        public DateTime DatePublished { get; set; }
     }
 }
